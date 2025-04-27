e2043aa7b70588b3990ffba75aa7d1a948c5d0fb
JAMES RYAN <james.paul9889@gmail.com>
2025-04-27 15:46:32 -0400

    updated app build scripts

--- proposal/build.sh
+++ proposal/build.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env bash
+# To be run as a bootstrap for the microservice on each machine.
 
-if [[ -z ~/go/bin/proto-gen-go-grpc ]]; then
-    echo Please make sure Go is installed, and run
-    echo -e '\t$ go install google.golang.org/protobuf/cmd/protoc-gen-go@latest'
-    echo -e '\t$ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest'
-fi
-
-# add protoc to path
-if [[ -z ~/go/bin/proto-gen-go ]]; then
-    ln -s $HOME/go/bin/protoc-gen-go-grpc $HOME/go/bin/protoc-gen-go
-fi
-
-# include gopath if not already included
-export GOPATH=$HOME/go
-case :$PATH:
-    in  *:$GOPATH/bin:*) ;;
-    *) export PATH=$PATH:$GOPATH/bin;;
-esac
-
-protoc --go-grpc_out=./vendor/engine \
-    node.proto
+# build docker container
+cd demo-app
+bash build-and-run.sh || (\
+    echo "============="
+    echo "Build failed!"
+    echo "============="
+    exit 1)
---
+++ proposal/demo-app/docker-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build -t test:latest .
+
+# to run
+#docker run -p 127.0.0.1:8000:8000 test:latest
