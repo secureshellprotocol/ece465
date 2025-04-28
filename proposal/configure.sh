63989a6d5b1a9bcc8261b085970c043ce6f231b4
JAMES RYAN <james.paul9889@gmail.com>
2025-04-27 20:22:49 -0400

    set up VM to be provisioned with docker
    TODO: make provision directory, like how geerling describes

---
+++ proposal/configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ensure ansible, vagrant are installed
+
+## gen a private ssh key
+#if [ ! -d ./.vagrant.d ]; then
+#    mkdir ./.vagrant.d
+#fi
+#
+#if [ ! -e ./.vagrant.d/worker_key ]; then
+#    ssh-keygen -t ed25519 -f ./.vagrant.d/worker_key -N ""
+#else
+#    echo "SSH Key exists for hosts already."
+#fi
+#echo "Done generating SSH Key."
+
+# install ansible requirements
+ansible-galaxy role install -r requirements.yml
+echo "Done setting up ansible-galaxy dependencies"
