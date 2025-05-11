363d82da469007157936ce8b1f0d71d92e7f8716
JAMES RYAN <james.paul9889@gmail.com>
2025-05-11 00:08:48 -0400

    continuing the provisioning

--- proposal/provisioning/demo-app/main.py
+++ proposal/provisioning/demo-app/main.py
@@ -4,7 +4,9 @@ from multiprocessing import Value
 # future work -- test atomicity with Redis support. This would probably be in a
 # sepearate container, if anything.
 counter = Value('i', 0)
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -14,4 +16,6 @@ def index():
 
     return jsonify(count=out)
 
-app.run(processes=1)
+
+if __name__ == "__main__":
+    app.run(processes=1, host='0.0.0.0', port=8000)
